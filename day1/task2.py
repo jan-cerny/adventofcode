767c93b85653a9455d88931afe5ce35ef6665cc3
Jan Černý <jcerny@redhat.com>
2019-12-12 20:12:16 +0100

    Day1

---
+++ day1/task2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+import math
+
+def count_fuel(mass):
+    result = math.floor(mass / 3) - 2
+    return result
+
+sum = 0
+with open("input", "r") as f:
+    for line in f:
+        mass = int(line)
+        while True:
+            fuel = count_fuel(mass)
+            if fuel <= 0:
+                break
+            sum += fuel
+            mass = fuel
+print(sum)
